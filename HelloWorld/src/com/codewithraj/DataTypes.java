88cbf837f17c886fbb8371cf10c9e087147adf79
Aman <amanrajsingh7870865175@gmail.com>
2025-08-04 15:04:28 +0530

    Data Types

---
+++ HelloWorld/src/com/codewithraj/DataTypes.java
@@ -0,0 +1,14 @@
+package com.codewithraj;
+
+public class DataTypes {
+    int age=10;
+    double weight=55.6;
+    float height=12.5f;
+    char gender='M';
+    boolean isMale=true;
+
+    public void informationPrint()
+    {
+        System.out.println(age+" "+weight+" "+height+" "+gender+" "+isMale);
+    }
+}
--- HelloWorld/src/com/codewithraj/Main.java
+++ HelloWorld/src/com/codewithraj/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.print("Welcome to Raj Code Universe");
+        System.out.println();
+        DataTypes data = new DataTypes();
+        data.informationPrint();
 
     }
 }
